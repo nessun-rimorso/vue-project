233451eff3a7a9cdfa44e1489d1242d96c067937
nessun <7planeta@bk.ru>
2019-11-30 19:11:35 +0300

    vue-resource

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
 <template>
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav">
-          <router-link class="nav-item" exact tag="li" to="/" active-class="active">
-            <a class="nav-link">Home</a>
-          </router-link>
-          <router-link class="nav-item" tag="li" :to="'/cars'" active-class="active">
-            <a class="nav-link">Cars</a>
-          </router-link>
+  <div class="container pt-2">
+    <div class="form-group">
+      <label for="name">Car name</label>
+      <input class="form-control" id="name" type="text" v-model.trim="carName">
+    </div>
 
-          <router-link class="nav-item" tag="li" to="/car/3" active-class="active">
-            <a class="nav-link">Car 3</a>
-          </router-link>
-          <router-link class="nav-item" tag="li" to="/car/4" active-class="active">
-            <a class="nav-link">Car 4</a>
-          </router-link>
-        </ul>
-      </div>
-    </nav>
-    <router-view></router-view>
+    <div class="form-group">
+      <label for="year">Car year</label>
+      <input class="form-control" id="year" type="text" v-model.number="carYear">
+    </div>
+
+    <button class="btn btn-success" @click="createCar">Create car</button>
+    <button class="btn btn-primary" @click="loadCars">Load cars</button>
+
+    <hr>
+
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        v-for="car of cars"
+        :key="car.id">
+        <strong>{{car.name}}</strong> - {{car.year}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      carName: ``,
+      carYear: 2018,
+      cars: [],
+      resource: null
+    }
+  },
+  methods: {
+    createCar () {
+      const car = {
+        name: this.carName,
+        year: this.carYear
+      }
+
+      this.resource.save({}, car)
+    },
+    loadCars () {
+      this.resource.get().then(response => response.json())
+        .then(cars => {
+          this.cars = cars
+        })
+    }
+  },
+  created () {
+    this.resource = this.$resource('cars')
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
+import VueResourse from 'vue-resource'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import router from './routes'
 
 Vue.config.productionTip = false
-
-Vue.use(VueRouter)
+Vue.use(VueResourse)
+Vue.http.options.root = 'http://localhost:3000/'
+Vue.http.interceptors.push(request => {
+  request.headers.set('Auth', 'RAND TOKEN ' + Math.random())
+})
 
 new Vue({
-  render: h => h(App),
-  router
+  render: h => h(App)
 }).$mount('#app')
